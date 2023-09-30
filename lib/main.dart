b333b12e2e39eeed9ff12688db5b126dd1515ee7
yasin <yasinmiapolash@gmail.com>
2023-09-30 23:57:11 +0600

    assignment

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,125 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Image Feed',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ImageFeed(),
+      home: HomeScreen(),
     );
   }
 }
 
-class ImageFeed extends StatelessWidget {
-  final List<String> dummyImages = [
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-  ];
-
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final orientation = MediaQuery.of(context).orientation;
-
     return Scaffold(
       appBar: AppBar(
-        title: const Text('News Feed'),
+        title: Text('My App'),
       ),
-      body: orientation == Orientation.portrait
-          ? buildListView()
-          : buildGridView(),
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          return ListView(
+            children: [
+              orientation == Orientation.portrait
+                  ? _buildPortraitLayout()
+                  : _buildLandscapeLayout(),
+            ],
+          );
+        },
+      ),
     );
   }
 
-  Widget buildListView() {
-    return ListView.builder(
-      itemCount: dummyImages.length,
-      itemBuilder: (context, index) {
-        return SizedBox(
-          height: 150,
-          width: 150,
-          child: Card(
-
-            margin: const EdgeInsets.all(8.0),
-            child: Image.network(
-
-                dummyImages[index]),
+  Widget _buildPortraitLayout() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: CircleAvatar(
+            radius: 200.0,
+            backgroundImage: NetworkImage('https://images.pexels.com/photos/3454835/pexels-photo-3454835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
           ),
-        );
-      },
+        ),
+        const Text(
+          'Yasin Mia ',
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'The "RenderFlex overflowed" error occurs when the content within a Column exceeds the available vertical space. ',
+          style: TextStyle(
+            fontSize: 16.0,
+
+          ),
+        ),
+        SizedBox(height: 16.0),
+        _buildImageGrid(2),
+      ],
     );
   }
 
-  Widget buildGridView() {
-    return GridView.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2, // Adjust this based on your requirements
+  Widget _buildLandscapeLayout() {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: CircleAvatar(
+            radius: 130.0,
+            backgroundImage: NetworkImage('https://images.pexels.com/photos/3454835/pexels-photo-3454835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Yasin Mia',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'The "RenderFlex overflowed" error occurs when the content within a Column exceeds the available vertical space. ',
+                style: TextStyle(
+                  fontSize: 16.0,
+
+                ),
+              ),
+              SizedBox(height: 16.0),
+              _buildImageGrid(3),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildImageGrid(int columnCount) {
+    return SizedBox(
+      height: 250,
+      width: 300,
+      child: GridView.builder(
+        shrinkWrap: true,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          mainAxisSpacing: 3,
+          crossAxisSpacing: 3,
+          crossAxisCount: 3,
+        ),
+        itemCount: 6,
+        itemBuilder: (BuildContext context, int index) {
+          final imageUrl = 'https://images.pexels.com/photos/1975069/pexels-photo-1975069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1$index';
+          return Image.network(imageUrl);
+        },
       ),
-      itemCount: dummyImages.length,
-      itemBuilder: (context, index) {
-        return Card(
-          margin: EdgeInsets.all(8.0),
-          child: Image.network(dummyImages[index]),
-        );
-      },
     );
   }
 }
